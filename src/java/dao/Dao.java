6e61c3480e08c089ba8cee7d9dd7a9b46df9f2b5
Nguyễn Trọng Đạt <124783881+AnhDat0103@users.noreply.github.com>
2025-01-26 22:50:28 +0700

    Create user class. Connect DB.

---
+++ src/java/dao/Dao.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+
+import java.util.List;
+
+/**
+ *
+ * @author DAT
+ * @param <T>
+ */
+public interface Dao<T> {
+     
+    public void save(T t);
+    
+    public List<T> findAll();
+    
+    public int update(T t);
+    
+    public int delete(int t);
+}
--- src/java/model/User.java
+++ src/java/model/User.java
@@ -4,10 +4,93 @@
  */
 package model;
 
+import model.enums.RoleEnums;
+
 /**
  *
  * @author DAT
  */
 public class User {
+    private int userId;
+    private String fullName;
+    private String email;
+    private String password;
+    private RoleEnums role;
+    private String phone;
+    private String address;
+
+    public User() {
+    }
+
+    public User(int userId, String fullName, String email, String password, RoleEnums role, String phone, String address) {
+        this.userId = userId;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public RoleEnums getRole() {
+        return role;
+    }
+
+    public void setRole(RoleEnums role) {
+        this.role = role;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "userId=" + userId + ", fullName=" + fullName + ", email=" + email + ", password=" + password + ", role=" + role + ", phone=" + phone + ", address=" + address + '}';
+    }
     
 }
