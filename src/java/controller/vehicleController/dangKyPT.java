f5a03badcbb83efca11ecc969631a9d5854c2342
ktuanvxhg28 <kdtuan28@gmail.com>
2025-03-19 22:45:25 +0700

    update check 1

--- src/java/controller/vehicleController/dangKyPT.java
+++ src/java/controller/vehicleController/dangKyPT.java
@@ -13,7 +13,6 @@ import validation.Validate;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
